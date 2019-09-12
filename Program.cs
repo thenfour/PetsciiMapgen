98fd5d2236279a0943290607a8c2504b2d58ae96
Carl Corcoran <carlco@gmail.com>
2019-09-12 18:31:14 +0200

    adding the tooll operator files which contain the frag shader portains of this

--- Program.cs
+++ Program.cs
@@ -28,6 +28,7 @@ namespace PetsciiMapgen
       //PetsciiMap map = new PetsciiMap("..\\..\\img\\fonts\\c64_all_onlyblock.gif", new Size(8, 8), new Size(2, 2), 16);
       //PetsciiMap map = new PetsciiMap("..\\..\\img\\fonts\\emoji-unknown-dark-24.png", new Size(24, 24), new Size(1, 1), 512);
       //var map = new HybridMap("..\\..\\img\\fonts\\emoji-unknown-dark-12.png", new Size(12, 12), new Size(2,2), 6, 0.6, 0.4);
+      var map = new HybridMap("..\\..\\img\\fonts\\emoji-unknown-dark-24.png", new Size(24, 24), new Size(2,2), 6, 0.66, 0.33);
       //var map = new HSVMapgen.PetsciiMap("..\\..\\img\\fonts\\pantoneswatches-8x8.png", new Size(8, 8), 16);
       //PetsciiMap map = new PetsciiMap("..\\..\\img\\fonts\\c64_all.gif", new Size(8, 8), new Size(1, 1), 256);
       //PetsciiMap map = new PetsciiMap("..\\..\\img\\fonts\\test-16blocks8x8.png", new Size(8, 8), new Size(2, 2), 2);
@@ -37,7 +38,7 @@ namespace PetsciiMapgen
       //PetsciiMap map = new PetsciiMap("..\\..\\img\\fonts\\Vga-rom-font-RVS.png", new Size(8, 16), new Size(2, 2), 16);
       //PetsciiMap map = new PetsciiMap("..\\..\\img\\fonts\\VGA-Boxonly.png", new Size(8, 16), new Size(2, 2), 16);
 
-      var map = HybridMap.Load("..\\..\\img\\maps\\mapHybrid-emoji12x12-2x2x6.png", new Size(12, 12), new Size(2, 2), 6);
+      //var map = HybridMap.Load("..\\..\\img\\maps\\mapHybrid-emoji12x12-2x2x6.png", new Size(12, 12), new Size(2, 2), 6);
 
       map.PETSCIIIZE("..\\..\\img\\airplane.jpg", "..\\..\\img\\testdest-airplane.png", false);
       map.PETSCIIIZE("..\\..\\img\\circle.png", "..\\..\\img\\testdest-circle.png", false);
